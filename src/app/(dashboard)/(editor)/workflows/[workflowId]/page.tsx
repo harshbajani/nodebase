89aa6f5b71e173b3342f2a9c05d05ac9dc6f16dc
harshbajani <harshbajani7@gmail.com>
2025-10-17 14:43:27 +0530

    Implement sidebar layout

---
+++ src/app/(dashboard)/(editor)/workflows/[workflowId]/page.tsx
@@ -0,0 +1,13 @@
+import { requireAuth } from "@/lib/auth-utils";
+import React from "react";
+
+interface PageProps {
+  params: Promise<{ workflowId: string }>;
+}
+const page = async ({ params }: PageProps) => {
+  await requireAuth();
+  const { workflowId } = await params;
+  return <div>{workflowId}</div>;
+};
+
+export default page;

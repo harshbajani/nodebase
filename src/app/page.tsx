3ed5dae2e751ccf48c0303dcd1b9b06b6074c535
harshbajani <harshbajani7@gmail.com>
2025-10-17 12:40:17 +0530

    Implement AI providers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { requireAuth } from "@/lib/auth-utils";
 import { useTRPC } from "@/trpc/client";
 import { caller } from "@/trpc/server";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { toast } from "sonner";
 
 const Page = () => {
   const trpc = useTRPC();
@@ -17,12 +18,24 @@ const Page = () => {
       },
     })
   );
+
+  const testAi = useMutation(
+    trpc.testAI.mutationOptions({
+      onSuccess: () => {
+        toast.success("AI job queued");
+      },
+    })
+  );
+
   return (
     <div className="flex items-center justify-center min-h-screen min-w-screen flex-col">
       Protected server component
       <div>{JSON.stringify(data, null, 2)}</div>
       <Button disabled={create.isPending} onClick={() => create.mutate()}>
         create Workflow
+      </Button>
+      <Button disabled={testAi.isPending} onClick={() => testAi.mutate()}>
+        gemini
       </Button>
     </div>
   );
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,10 +1,19 @@
 import prisma from "@/lib/database";
 import { createTRPCRouter, protectedProcedure } from "../init";
 import { inngest } from "@/inngest/client";
+import { generateText } from "ai";
+import { google } from "@ai-sdk/google";
+
 export const appRouter = createTRPCRouter({
+  testAI: protectedProcedure.mutation(async () => {
+    await inngest.send({ name: "execute/ai" });
+    return { success: true, message: "Job queued" };
+  }),
+
   getWorkflows: protectedProcedure.query(({ ctx }) => {
     return prisma.workflow.findMany();
   }),
+
   createWorkflow: protectedProcedure.mutation(async () => {
     await inngest.send({
       name: "test/hello.world",

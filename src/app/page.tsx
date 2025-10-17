13b757b50462ae5d4de75de0c4dada38afbf5dd9
harshbajani <harshbajani7@gmail.com>
2025-10-17 11:32:35 +0530

    Implement inngest workflows and background jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,29 @@
+"use client";
+import { Button } from "@/components/ui/button";
 import { requireAuth } from "@/lib/auth-utils";
+import { useTRPC } from "@/trpc/client";
 import { caller } from "@/trpc/server";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
-const Page = async () => {
-  await requireAuth();
-  const data = await caller.getUsers();
+const Page = () => {
+  const trpc = useTRPC();
+  const queryClient = useQueryClient();
+  const { data } = useQuery(trpc.getWorkflows.queryOptions());
+
+  const create = useMutation(
+    trpc.createWorkflow.mutationOptions({
+      onSuccess: () => {
+        queryClient.invalidateQueries(trpc.getWorkflows.queryOptions());
+      },
+    })
+  );
   return (
     <div className="flex items-center justify-center min-h-screen min-w-screen flex-col">
       Protected server component
       <div>{JSON.stringify(data, null, 2)}</div>
+      <Button disabled={create.isPending} onClick={() => create.mutate()}>
+        create Workflow
+      </Button>
     </div>
   );
 };

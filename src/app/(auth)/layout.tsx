96877b6be30b5d044b72beaf93afe77b4e7580eb
harshbajani <harshbajani7@gmail.com>
2025-10-17 10:20:35 +0530

    Implement theme and styling

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,9 @@
+import { AuthLayout } from "@/features/auth/ui/components/auth-layout";
+
+import React from "react";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return <AuthLayout>{children}</AuthLayout>;
+};
+
+export default Layout;

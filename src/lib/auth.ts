eba63bacf371fa0cefa3771ba1d61726f4a23a5c
harshbajani <harshbajani7@gmail.com>
2025-12-02 23:21:25 +0530

    Implement social authentication

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,7 +2,6 @@ import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "./database";
 import { polar, checkout, portal } from "@polar-sh/better-auth";
-import { Polar } from "@polar-sh/sdk";
 import { polarClient } from "./polar";
 
 export const auth = betterAuth({
@@ -12,6 +11,16 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
     autoSignIn: true,
+  },
+  socialProviders: {
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    },
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
   },
   plugins: [
     polar({
